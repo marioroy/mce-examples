a9de757c8ced5d085cae295a0100aebfdfc61fe3
Mario Roy <marioeroy@gmail.com>
2017-02-13 00:47:32 -0600

    Optimized the hobo worker script using Gearman::XS.

--- gearman_xs/reverse_worker_hobo.pl
+++ gearman_xs/reverse_worker_hobo.pl
@@ -47,27 +47,56 @@ sub reverse {
    my ($job, $options) = @_;
    my $workload = thaw( $job->workload() ); # [ chunk_id, chunk_ref ]
 
-   my $seq  = MCE::Shared->sequence(0, $#{ $workload->[1] });
+   my $chunk_size = 500;
+      $chunk_size =  50 if ( @{ $workload->[1] } < 1500 );
+      $chunk_size =   5 if ( @{ $workload->[1] } <  150 );
+
+   my $seq  = MCE::Shared->sequence(
+      { bounds_only => 1, chunk_size => 500 }, 0, $#{ $workload->[1] }
+   );
+
+   # The bounds_only option is important to have the shared-manager process
+   # send the next begin and end values only. Because, it's faster to have
+   # the worker do a for ( $beg .. $end ) loop versus obtain each sequence
+   # number individualy one at a time.
+
    my $data = MCE::Shared->array();
 
    my $parallel = sub {
-      while ( defined ( my $pos = $seq->next ) ) {
-         my $string = $workload->[1][$pos]; chomp($string);
-         my $string_size = length($string);
-         my $result = '';
 
-         for (my $i = $string_size; $i > 0; $i--) {
-            my $letter = substr($string, ($i - 1), 1);
-            $result .= $letter;
+      while ( my ($beg, $end) = $seq->next ) {
+         my %_data;
+
+         for my $pos ($beg .. $end) {
+            my $string = $workload->[1][$pos]; chomp($string);
+            my $string_size = length($string);
+            my $result = '';
+
+            for (my $i = $string_size; $i > 0; $i--) {
+               my $letter = substr($string, ($i - 1), 1);
+               $result .= $letter;
+            }
+
+          # $data->set($pos, "$string: $result\n"); # don't do this
+
+            $_data{$pos} = "$string: $result\n"; # store locally instead
          }
 
-         $data->set($pos, "$string: $result\n");
+         $data->mset(%_data); # batch update shared-array outside of loop
       }
+
+      # Working with shared data among workers can be fast if batching
+      # requests here and there. The result is a reduction in IPC to and
+      # from the shared-manager process which is a good thing.
+
+      # Both MCE and MCE::Shared were built with batching capabilities.
+      # So, my friend, why not take advantage of it when you can.
 
       return;
    };
 
    MCE::Hobo->create($parallel) for (1 .. 4);
+
    MCE::Hobo->waitall();
 
    printf( "Job=%s ChunkID=%s NumItems=%s\n",
